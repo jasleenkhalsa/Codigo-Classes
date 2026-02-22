450715c34037d90f201c88ad6ffbf22d9c0eb7c7
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-02-23 00:12:29 +0530

    Added Book class

---
+++ Library_Management_System/main.py
@@ -0,0 +1,7 @@
+# Library Management System
+
+class Books:
+    def __init__(self, book_id, book_name, author_name):
+        self.book_id = book_id
+        self.book_name = book_name
+        self.author_name = author_name
