e7a6ef404adf2bce9130e520db4018c48364e550
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-02-23 12:42:29 +0530

    Added Book Unit Class

--- Library_Management_System/main.py
+++ Library_Management_System/main.py
@@ -1,7 +1,15 @@
 # Library Management System
 
+# BOOKS
 class Books:
     def __init__(self, book_id, book_name, author_name):
         self.book_id = book_id
         self.book_name = book_name
         self.author_name = author_name
+
+# BOOK UNITS
+class BookUnit:
+    def __init__(self, book_name, total_unit, available_unit):
+        self.book_name = book_name
+        self.total_unit = total_unit
+        self.available_unit = available_unit
