9dcf9722cb6797fb730eb60b387a93b64296a86e
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-02-23 12:48:29 +0530

    Added Issue Table

--- Library_Management_System/main.py
+++ Library_Management_System/main.py
@@ -13,3 +13,12 @@ class BookUnit:
         self.book_name = book_name
         self.total_unit = total_unit
         self.available_unit = available_unit
+
+# ISSUE TABLE
+class IssueRecord:
+    def __init__(self, user_id, book_id, issue_date, return_date):
+        self.user_id = user_id
+        self.book_id = book_id
+        self.issue_date = issue_date
+        self.return_date = return_date
+
