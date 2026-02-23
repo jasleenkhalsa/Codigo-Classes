90320ccbac1a335970907c07feebad5f150ec893
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-02-23 15:17:04 +0530

    Functions Added

--- Library_Management_System/main.py
+++ Library_Management_System/main.py
@@ -61,16 +61,49 @@ class Admin(User):
     def __init__(self, user_id, name, year, email, mobile):
         super().__init__(user_id, name, year, email, mobile, True)
 
-   
+# Store Data
+
+books = {}
+users = {}
+history_record = {}
+
+# Functions
+
+def add_book():
+    num = int(input("Enter Number of Book entries you want to ADD :"))
+    for i in range (num):
+        book_id = input("Enter Book ID: ")
+        book_name = input("Enter Book Name: ")
+        author_name = input("Enter Author Name: ")
+
+        books[book_id] = Book(book_id, book_name, author_name)
+
+        print("Book Added Successfully!\n")
+
+def add_user():
+    num = int(input("Enter Number of Users you want to ADD: "))
+
+    for i in range(num):
+        user_id = input("Enter User ID: ")
+        name = input("Enter Name: ")
+        year = input("Enter Year: ")
+        email = input("Enter Email: ")
+        mobile = input("Enter Mobile: ")
+
+        is_admin = input("Is Admin (True/False): ").lower() == "true"
+
+        users[user_id] = User(user_id, name, year, email, mobile, is_admin)
+
+        print("User Added Successfully!\n")
+
+
 
 # Library    
 while True:
     print("\n Library Menu ")
     print("1. Add Book")
     print("2. Add User")
-    print("3. View Books")
-    print("4. View Users")
-    print("5. Exit")
+    print("3. Exit")
 
     choice = input("Enter choice: ")
 
@@ -79,10 +112,6 @@ while True:
     elif choice == "2":
         add_user()
     elif choice == "3":
-        view_books()
-    elif choice == "4":
-        view_users()
-    elif choice == "5":
         print("Exiting System...")
         break
     else:
