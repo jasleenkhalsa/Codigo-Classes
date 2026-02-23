13e4e917dfd5b2718417c29b206701e1d8dc9cf4
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-02-23 15:03:04 +0530

    Remove ip table and add lib menu

--- Library_Management_System/main.py
+++ Library_Management_System/main.py
@@ -2,30 +2,41 @@
 
 # BOOKS
 class Book:
+
+
     def __init__(self, book_id, book_name, author_name):
         self.book_id = book_id
         self.book_name = book_name
         self.author_name = author_name
+
     def display_info(self):
         print(self.book_id, self.book_name, self.author_name)    
 
 # BOOK UNITS
 class BookUnit:
+
+
     def __init__(self, book, total_unit, available_unit):
         self.book = book
         self.total_unit = total_unit
         self.available_unit = available_unit
 
+
 # ISSUE TABLE
 class IssueRecord:
+
+
     def __init__(self, user_id, book_id, issue_date, return_date):
         self.user_id = user_id
         self.book_id = book_id
         self.issue_date = issue_date
         self.return_date = return_date
 
+
 # USER INFO
 class User:
+
+
     def __init__(self, user_id, name, year, email, mobile, is_admin):
         self.user_id = user_id
         self.name = name
@@ -33,6 +44,9 @@ class User:
         self.email = email
         self.mobile = mobile
         self.is_admin = is_admin
+
+    def display_info(self):
+        print(f"ID: {self.user_id}, Name: {self.name}, Admin: {self.is_admin}")
 
 # HISTORY TABLE
 class History:
@@ -49,59 +63,28 @@ class Admin(User):
 
    
 
-# Input for Tables
-# books info input
-books = {}
+# Library    
+while True:
+    print("\n Library Menu ")
+    print("1. Add Book")
+    print("2. Add User")
+    print("3. View Books")
+    print("4. View Users")
+    print("5. Exit")
 
-for i in range(5):
-    book_id = input("Enter Book ID: ")
-    book_name = input("Enter Book Name: ")
-    author_name = input("Enter Author Name: ")
+    choice = input("Enter choice: ")
 
-    books[book_id]={
-        "book_name" : book_name,
-        "author_name" : author_name
-    }
+    if choice == "1":
+        add_book()
+    elif choice == "2":
+        add_user()
+    elif choice == "3":
+        view_books()
+    elif choice == "4":
+        view_users()
+    elif choice == "5":
+        print("Exiting System...")
+        break
+    else:
+        print("Invalid Choice!")
 
-print("\nBooks Data:")
-print(books)
-
-#  user info input
-users = {}
-
-for _ in range(5):
-    user_id = input("Enter User ID: ")
-    name = input("Enter Name: ")
-    year = input("Enter Year: ")
-    email = input("Enter Email: ")
-    mobile = input("Enter Mobile: ")
-    is_admin = input("Is Admin (True/False): ")
-
-    users[user_id] = {
-        "name": name,
-        "year": year,
-        "email": email,
-        "mobile": mobile,
-        "is_admin": is_admin
-    }
-
-print("\nUsers Data:")
-print(users)
-
-# History info
-history = {}
-
-for _ in range(5):
-    user_id = input("Enter User ID: ")
-    book_name = input("Enter Book Name: ")
-    issue_date = input("Enter Issue Date: ")
-    return_date = input("Enter Return Date: ")
-
-    history[user_id] = {
-        "book_name": book_name,
-        "issue_date": issue_date,
-        "return_date": return_date
-    }
-
-print("\nHistory Data:")
-print(history)
