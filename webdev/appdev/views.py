398d04f064163f82710ec45f2d1281fdd369794f
Jasleen kaur Khalsa <jasleenkhalsa2005@gmail.com>
2026-03-03 15:54:21 +0530

    Uploading and Displaying Static Files

--- webdev/appdev/views.py
+++ webdev/appdev/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render,HttpResponse, redirect
 from appdev.models import TableOne
 
 def home(request):
+    user_data = TableOne.objects.all()
+
     if request.method == 'POST':
         name = request.POST.get('var_name')
         mobile = request.POST.get('mobile')
         email = request.POST.get('email')
-        
-        TableOne.objects.create(name = name, mobile=mobile, email = email)
-        return redirect('/')
+        file = request.FILES.get('file')
 
-     #   print(f"Name: {name}, Mobile: {mobile}, Email: {email}")
-    return render(request, 'home.html')
+        TableOne.objects.create(name = name, mobile=mobile, email = email, file = file)
+        return redirect('/')
+    # json -> {dict}
+    # data_front = {
+      #   'data': user_data}
     
 
+     #   print(f"Name: {name}, Mobile: {mobile}, Email: {email}")
+    return render(request, 'home.html', {'data' : user_data})
+    
 
-
-# Create your views here.
+'''
+data
+'''
